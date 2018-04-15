2212211ee74b581f724026f1e64bf969678765a9
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-15 02:41:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,8 @@
 var playlist = {TaylorSwift : `Blank Space`}
 function updatePlaylist(playlist,artist,songTitle){
   playlist[artist]=songTitle
-  delete playlist.Slowdive
 }
 function removeFromPlaylist(playlist, artistName){
 
-  delete playlist.artistName
+  delete playlist.Slowdive
 }
