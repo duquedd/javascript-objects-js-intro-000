8eb3a4adcd922fcfad4b61e6df6890f8c47cda3a
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-15 02:06:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {}
 function updatePlaylist(playlist,artist,songTitle){
   return Object.assign({},playlist,{[artist]: songTitle})
 }
-function removeFromPlaylist(playlist,`artist`){
+function removeFromPlaylist(playlist,artist){
   delete plalist.artist
 }
