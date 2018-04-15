c799a353396f9539e8287437e73c227f1b8e1ed9
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-15 02:08:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {TaylorSwift : Blank Space}
+var playlist = {TaylorSwift : `Blank Space`}
 function updatePlaylist(playlist,artist,songTitle){
   return Object.assign({},playlist,{[artist]: songTitle})
 }
